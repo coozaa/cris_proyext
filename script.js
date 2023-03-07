f9b880bb7b4f6d92ea499b3253a13742166c68d6
cristian Torres <coozaa83@gmail.com>
2023-03-07 16:10:53 -0300

    07 marz 4:10

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu() {
+    document.querySelector("menu__conteiner").classList.toggle("mostrar_menu");
+}
